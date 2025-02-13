fa5591d913c2e2328eadc804f1f88acfa33b41ca
Shivam Tiwari <shivam.tiwari.m1@gmail.com>
2025-02-13 16:55:44 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -14,11 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
+mongoose.connect(process.env.MONGO_URI);
 
 // API Routes
 app.post("/add", (req, res) => {
