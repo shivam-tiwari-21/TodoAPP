0ce2bf9d36cb68d1518f8c1bf8b88aa44449d306
Shivam Tiwari <shivam.tiwari.m1@gmail.com>
2025-02-13 17:14:32 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -49,10 +49,10 @@ app.delete('/delete/:id', async (req, res) => {
 
 // **Serve Vite Frontend in Production**
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../Frontend/dist", "index.html"));
 });
 
 // Start Server
